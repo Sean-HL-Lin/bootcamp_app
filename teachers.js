b8b3427a549e2e3458e960c7739da84999da351f
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-07-25 01:58:51 +0000

    added code for preventing sql injection

--- teachers.js
+++ teachers.js
@@ -11,17 +11,16 @@ const pool = new Pool({
 pool.connect();
 
 
-const cohort = process.argv[2]
-
+const cohort = [`%${process.argv[2]}%`]
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, cohort)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
